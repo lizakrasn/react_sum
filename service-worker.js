eb876afa60dbc0ae866c77d7c82b9dfb8682444a
Liza Krasnoshchok <nlight1122@gmail.com>
2020-10-21 19:07:04 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.90f84bb0562c218e8072126c65d96c3c.js"
 );
 
 self.addEventListener('message', (event) => {
